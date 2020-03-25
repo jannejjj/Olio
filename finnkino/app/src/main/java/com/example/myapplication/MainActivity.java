e9e562864ea870d410c5072b6e362ef1b7db9e39
jannejjj <61980297+jannejjj@users.noreply.github.com>
2020-03-25 12:02:19 +0200

    .

--- finnkino/app/src/main/java/com/example/myapplication/MainActivity.java
+++ finnkino/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void readXML() {
-       // finnkino.readXML_FI();
+        finnkino.readXML_FI();
         finnkino.readXML_EST();
     }
 
